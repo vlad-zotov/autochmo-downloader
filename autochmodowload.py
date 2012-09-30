ce0a8e678722929fab124ad5f921f6cbac262095
Myname <me@horse.(none)>
2012-09-30 11:05:15 +0400

    grab all site + some improvements

--- autochmodowload.py
+++ autochmodowload.py
@@ -1,47 +1,47 @@
 ﻿from httplib import *
 from xml.dom.minidom import *
-import os
+import os, sys
 import urllib2
 from urllib import urlretrieve
 
 def getPictures():
-	host = "xml.autochmo.ru"
-	h = HTTP(host)
-	h.putrequest("GET", "/")
-	h.putheader('Host', host)
-	h.putheader('User-agent', 'python-httplib')
-	h.endheaders()
-	h.getreply()
-	xml = parseString(h.getfile().read())
-	ids = [i.getAttribute('id') for i in xml.getElementsByTagName("fact")]
-	for id in ids:
-		h.putrequest("GET", "/"+str(id))
-		h.putheader('Host', host)
-		h.putheader('User-agent', 'python-httplib')
-		h.endheaders()
-		h.getreply()
-		xml = parseString(h.getfile().read())
-		elem = xml.getElementsByTagName("pictures")
-		try:
-			os.mkdir(str(id))
-			print "true"
-		except Exception as e:
-			print e
-		f = open(str(id)+"\\number.txt", "w")
-		number = xml.getElementsByTagName("gosnomer")[0].childNodes[0].nodeValue
-		try:
-			number = str(number)
-			f.write(number)
-		except:
-			pass
-		f.close()
-		for node in elem[0].getElementsByTagName("original")[0].getElementsByTagName("src"):
-			try:
-				name = str(node.childNodes[0].nodeValue).split('/')[-1]
-				urlretrieve("http://autochmo.ru"+node.childNodes[0].nodeValue, str(id) + "\\" +name)
-			except:
-				pass
-	print "That's all"
-	
+    host = "xml.autochmo.ru"
+    h = HTTP(host)
+    h.putrequest("GET", "/")
+    h.putheader('Host', host)
+    h.putheader('User-agent', 'python-httplib')
+    h.endheaders()
+    h.getreply()
+    xml = parseString(h.getfile().read())
+    ids = [int(i.getAttribute('id')) for i in xml.getElementsByTagName("fact")]
+    for id in range(max(ids),0,-1):
+        print "processing %d" % id
+        try:
+            h.putrequest("GET", "/"+str(id))
+            h.putheader('Host', host)
+            h.putheader('User-agent', 'python-httplib')
+            h.endheaders()
+            h.getreply()
+            xml = parseString(h.getfile().read())
+            elem = xml.getElementsByTagName("pictures")
+            number = xml.getElementsByTagName("gosnomer")[0].childNodes[0].nodeValue
+
+            for node in elem[0].getElementsByTagName("original")[0].getElementsByTagName("src"):
+                src = "http://autochmo.ru" + str(node.childNodes[0].nodeValue)
+                name = str(node.childNodes[0].nodeValue).split('/')[-1]
+                new_name = "data/%05d_%s" % (id, name)
+                print "\tget %s" % src
+                urlretrieve(src, new_name)
+
+        except KeyboardInterrupt:
+            raise
+        except:
+            print "\terror"
+            continue
+        print "\tok"
+        sys.stdout.flush()
+
 if __name__ == '__main__':
-	getPictures()
+    try: os.mkdir("data")
+    except: pass
+    getPictures()
